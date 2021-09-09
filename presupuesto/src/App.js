545c42b5c916b6467768be25e48305bd50e0a3ae
Uriel Quiroz <uriel.quiroz187@gmail.com>
2021-09-09 09:32:56 -0500

    Creación del proyecto
    Se creo el proyecto, se agrego el archivo css y archivos que se utilizaran en el proyecto

---
+++ presupuesto/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+
+    <div className="container">
+      <header>
+        <h1>Gasto Semanal</h1>
+      </header>
+    </div>
+
+  );
+}
+
+export default App;
