b24472c8337480479282c85064163fd33610f28a
Uriel Quiroz <uriel.quiroz187@gmail.com>
2021-09-09 10:11:34 -0500

    useState Error
    Se creo el state de error para validar que el usuario ingrese una cantidad valida

--- presupuesto/src/components/Pregunta.jsx
+++ presupuesto/src/components/Pregunta.jsx
@@ -3,16 +3,30 @@ import React, { Fragment, useState } from "react";
 const Pregunta = () => {
   //DEfinir presupuesto
   const [cantidad, guardarCantidad] = useState(0);
+  const [error, guardarError] = useState(false);
 
   //Funcion que lee el presupuesto
   const definirPresupuesto = e => {
     guardarCantidad(parseInt(e.target.value, 10))
   }
 
+  //Submit para definir el presupuesto
+  const agregarPresupuesto = e => {
+      e.preventDefault();
+
+      //Validar
+      if(cantidad < 1 || isNaN(cantidad)) {
+          guardarError(true);
+          return;
+      }
+
+      //Si pasa la validación
+  }
+
   return (
     <Fragment>
       <h2>Coloca tu presupuesto</h2>
-      <form>
+      <form onSubmit={agregarPresupuesto} >
         <input
           type="number"
           className="u-full-width"
