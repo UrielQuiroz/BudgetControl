f8ddb1496151299711700bb2330961457312febe
Uriel Quiroz <uriel.quiroz187@gmail.com>
2021-09-09 09:55:08 -0500

    useState del presupuesto
    Se creo un state para guardar el presupuesto del usuario

--- presupuesto/src/App.js
+++ presupuesto/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Pregunta from './components/Pregunta';
 
 function App() {
+
   return (
 
     <div className="container">
--- presupuesto/src/components/Pregunta.jsx
+++ presupuesto/src/components/Pregunta.jsx
@@ -1,13 +1,30 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 
 const Pregunta = () => {
+  //DEfinir presupuesto
+  const [cantidad, guardarCantidad] = useState(0);
+
+  //Funcion que lee el presupuesto
+  const definirPresupuesto = e => {
+    guardarCantidad(parseInt(e.target.value, 10))
+  }
+
   return (
     <Fragment>
       <h2>Coloca tu presupuesto</h2>
       <form>
-          <input type="number" className="u-full-width" placeholder="Coloca tu presupuesto" />
-          <input type="submit" className="button-primary u-full-width" value="Definir presupuesto" />
-      </form> 
+        <input
+          type="number"
+          className="u-full-width"
+          placeholder="Coloca tu presupuesto"
+          onChange={definirPresupuesto}
+        />
+        <input
+          type="submit"
+          className="button-primary u-full-width"
+          value="Definir presupuesto"
+        />
+      </form>
     </Fragment>
   );
 };
